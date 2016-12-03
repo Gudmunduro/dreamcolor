d40af8e4cb2f02d5b383c17ca6140c2462e9077f
Marcus Ramse <jerwuqu@gmail.com>
2016-12-03 16:59:18 +0100

    New CLI

--- cli.py
+++ cli.py
@@ -4,23 +4,32 @@ from dreamcolor import Light
 # Args
 parser = argparse.ArgumentParser()
 parser.add_argument("ip")
+parser.add_argument("action", choices=["on", "off", "dim", "rgb"])
+parser.add_argument("values", nargs=argparse.REMAINDER, type=int)
 parser.add_argument("--port", type=int)
-parser.add_argument("--dim",  type=int, choices=range(0, 11),  metavar="0-10")
-parser.add_argument("-r",     type=int, choices=range(0, 256), metavar="0-255")
-parser.add_argument("-g",     type=int, choices=range(0, 256), metavar="0-255")
-parser.add_argument("-b",     type=int, choices=range(0, 256), metavar="0-255")
 args = parser.parse_args()
 
 # Create
 light = Light(args.ip, args.port or 5000)
 
+# On/Off
+if args.action == "on":
+    light.on()
+elif args.action == "off":
+    light.off()
+
 # Dim
-if args.dim != None:
-    light.dim(args.dim)
+elif args.action == "dim":
+    if len(args.values) != 1 or not 0 <= args.values[0] <= 10:
+        print("Error: You need to supply a value 0-10")
+        exit()
+
+    light.dim(args.values[0])
 
 # RGB
-if args.r != None and args.g != None and args.b != None:
-    light.rgb(args.r, args.g, args.b)
-elif args.r != None or args.g != None or args.b != None:
-    print("Error: You need to supply all RGB arguments!")
-    exit()
+elif args.action == "rgb":
+    if len(args.values) != 3 or not all(0 <= v <= 255 for v in args.values):
+        print("Error: You need to supply three values 0-255")
+        exit()
+
+    light.rgb(args.values[0], args.values[1], args.values[2])

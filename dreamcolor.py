6f6422311977d3b6c0dcb72dcaf4619cc88c429d
gudmunduro <goh140400@gmail.com>
2017-06-21 13:36:13 +0000

    Added temperture, brightness, preset, speed and pattern

--- dreamcolor.py
+++ dreamcolor.py
@@ -15,12 +15,47 @@ class Light():
         """ Takes a number from 0-10 """
 
         self.__send([0x7e, 0x05, 0x03, 0x80 + a, 0x01, 0xff, 0xff, 0x00, 0xef])
+    
+    def temp(self, v=10):
+        """ Takes a number from 0-10 """
+
+        self.__send([0x7e, 0x05, 0x03, 0x80 + v, 0x02, 0xff, 0xff, 0x00, 0xef])
 
     def rgb(self, r, g, b, fade=True):
         """ Takes three numbers from 0-255 """
 
         fadeVal = 0x03 if fade else 0x04
         self.__send([0x7e, 0x07, 0x05, fadeVal, r, g, b, 0x00, 0xef])
+
+    def brightness(self, a=100):
+        """ Takes a number from 0-100 """
+
+        self.__send([0x7e, 0x04, 0x01, a, 0xff, 0xff, 0xff, 0x00, 0xef])
+    
+    def preset(self, mode):
+        """ Takes the id of the preset mode """
+
+        self.__send([0x7e, 0x05, 0x03, mode, 0x03, 0xff, 0xff, 0x00, 0xef])
+    
+    def pattern(self, pattern_type, pattern):
+        """ Takes the type and a list with rgb values like [r, g, b, r, g, b] etc """
+        """
+        Types:
+        Jumping = 0
+        Gradual = 1
+        Strobe = 2
+        """
+
+        byte_list = [0xaf, 0x10, pattern_type, int(len(pattern) / 3)]
+        for i in pattern:
+            byte_list.append(i)
+        print(byte_list)
+        self.__send(byte_list)
+    
+    def speed(self, s=200):
+        """ Takes a number from 0 - 200 """
+
+        self.__send([0x7e, 0x04, 0x02, s, 0xff, 0xff, 0xff, 0x00, 0xef])
 
     def on(self):
         """ Turns on the light """
